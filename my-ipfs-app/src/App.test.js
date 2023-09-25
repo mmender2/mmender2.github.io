5ea23f3d080fdb5ec47df58f25ee3fbeddf47b7a
Max <mmender2@gmail.com>
2023-09-25 13:17:05 -0500

    initial

--- my-ipfs-app/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn ipfs link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn ipfs/i);
-  expect(linkElement).toBeInTheDocument();
-});
